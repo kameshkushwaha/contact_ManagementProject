de8d406c6168c70deeeb8d4468d1b217e98f6fd9
Kamesh Kushwaha <kameshkushwaha2001@gmail.com>
2024-09-01 07:57:18 +0530

    firsst project commit

---
+++ ManagementProject/src/main/java/com/managent/project/ManagementProject/ManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.managent.project.ManagementProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementProjectApplication.class, args);
+	}
+
+}
---
+++ ManagementProject/src/test/java/com/managent/project/ManagementProject/ManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managent.project.ManagementProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
